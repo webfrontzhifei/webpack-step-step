98d8baf0afedd1843d0496725930769df478dd72
zhangzhifei <zhangzhifei@yuewen.com>
2017-07-14 19:39:46 +0800

    step2

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,3 +1,8 @@
 require('./check-versions')();
 
 var config = require('../config');
+console.log(config.dev.env.NODE_ENV);
+console.log(JSON.parse(config.dev.env.NODE_ENV));
+// if(!process.env.NODE_ENV) {
+//   process.env.NODE_ENV =
+// }
--- config/index.js
+++ config/index.js
@@ -1,1 +1,24 @@
-index.js
+var path = require('path');
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report,
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 8080,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    cssSourceMap: false,
+  }
+}
